a85e3f7f74cb9889191f8ac9adac36050c2e8895
Zyris Shakir <zeyeland@gmail.com>
2019-04-04 16:23:31 -0500

    Vanilla ajax

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,16 +1,7 @@
 var apod = {
     //Application Constructor
     init: function(){
-        var date = this.randomDate(new Date(1995,5,16), new Date());
-        var url = "https://api.nasa.gov/planetary/apod?api_key=" + nasaKey + "&date=" + date;
-
-        $.ajax({
-            url: url
-        }).done(function(result){
-          console.log(result);
-        }).fail(function(result){
-          console.log(result);
-        });
+        this.getRequest();
     },
     //Create a random date
     randomDate: function(start, end) {
@@ -33,8 +24,61 @@ var apod = {
     
         return `${y}-${m}-${d}`;
     },
+    getRequest: function(){
+        var _this = this;
+        var date = this.randomDate(new Date(1995,5,16), new Date());
+        //var date = "2013-06-06";
+        var url = "https://api.nasa.gov/planetary/apod?api_key=" + nasaKey + "&date=" + date;
+        $.ajax({
+            url: url
+        }).done(function(result){ 
+            console.log(result);
+            _this.buildDOM(result);
+            
+        }).fail(function(result){
+          console.log(result);
+        }); 
+    },
+    buildDOM: function(result){
+        //If the media type is video hide the image elements and display a video.
+        if(result.media_type === 'video') {
+            $("#apodImg").hide();
+            $("#apodVideo > iframe").attr("src", result.url).show();
+        }else{
+            $("#apodVideo").hide();
+            $("#apodImg").attr("src", result.url).attr('alt', result.title).show();
+        }
+        $("#apodTitle").text(result.title);
+        $("#apodCopyright").text("Copyright: " + result.copyright);
+        $("#apodDate").text("Date: " + result.date);
+        $("#apodDesc").text(result.explanation);
+    }
 };
 
 
+    $('#btnRandApod').on('click',function(){
+      apod.getRequest();
+    });
 
-apod.init();
+
+//apod.init();
+
+
+var date = "2013-06-06";
+var answer="";
+var url = "https://api.nasa.gov/planetary/apod?api_key=" + nasaKey + "&date=" + date;
+function loadDoc() {
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+        answer = this.responseText;
+        console.log(answer);
+        answer = JSON.parse(answer);
+        console.log(answer);
+    };
+    xhttp.open("GET", url, true);
+    xhttp.send(); 
+}
+
+loadDoc();
+
+
